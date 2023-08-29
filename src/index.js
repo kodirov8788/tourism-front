5b0a9200c29c70581cda456c3bf5a9ce3f450584
kodirov8788 <kodirov8788@gmail.com>
2023-08-29 09:47:49 +0500

    sdas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+
+        <Route path='*' element={<h1 className='text-center mt-10 text-5xl font-bold'>404 || Pages not found</h1>} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
