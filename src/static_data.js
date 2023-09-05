bca1d1923dd42cd803692f8b058688a417c21de1
YOSINBEK-uz <128227866+YOSINBEK-uz@users.noreply.github.com>
2023-09-05 15:58:42 +0500

    static-data changed

--- src/static_data.js
+++ src/static_data.js
@@ -1,110 +1,77 @@
-import Img1 from "./images/image-20.jpg";
-import Img2 from "./images/image-8.jpg";
-import Img3 from "./images/image-11.jpg";
-import Img4 from "./images/image-10.jpg";
-import Img5 from "./images/image-13.jpg";
-import Img6 from "./images/image-9.jpg";
-import Img7 from "./images/image-1.jpg";
-import Img8 from "./images/image-21.jpg";
-import Paketlar_img1 from "./pages/Paketlar/Paketlar_img/paketlar_img2.jpg";
-import Paketlar_img2 from "./pages/Paketlar/Paketlar_img/paketlar_img3.jpg";
-import Paketlar_img3 from "./pages/Paketlar/Paketlar_img/paketlar_img4.jpg";
-import Paketlar_img4 from "./pages/Paketlar/Paketlar_img/paketlar_img5.jpg";
-import Paketlar_img5 from "./pages/Paketlar/Paketlar_img/paketlar_img6.jpg";
-import Paketlar_img6 from "./pages/Paketlar/Paketlar_img/paketlar_img7.jpg";
-import Paketlar_img7 from "./pages/Paketlar/Paketlar_img/paketlar_img8.jpg";
+import paketlar_img2 from "./pages/Paketlar/Paketlar_img/paketlar_img2.jfif"
+import Img1 from "./images/image-20.jpg"
+import Img2 from "./images/image-8.jpg"
+import Img3 from "./images/image-11.jpg"
+import Img4 from "./images/image-10.jpg"
+import Img5 from "./images/image-13.jpg"
+import Img6 from "./images/image-9.jpg"
+import Img7 from "./images/image-1.jpg"
+import Img8 from "./images/image-21.jpg"
+import paketlar_img2 from "./pages/Paketlar/Paketlar_img/paketlar_img2.jfif"
+import Img1 from "./images/image-20.jpg"
+import Img2 from "./images/image-8.jpg"
+import Img3 from "./images/image-11.jpg"
+import Img4 from "./images/image-10.jpg"
+import Img5 from "./images/image-13.jpg"
+import Img6 from "./images/image-9.jpg"
+import Img7 from "./images/image-1.jpg"
+import Img8 from "./images/image-21.jpg"
 
 export const Turistik_paketlar = [
-  {
-    id: 1,
-    img: Img1,
-    text: "O'zbekiston bo'ylab sayohat",
-    price: "4800000 uzs",
-  },
-  {
-    id: 2,
-    img: Img2,
-    text: "Dubaiga sayohat",
-    price: "17400000 uzs",
-  },
-  {
-    id: 3,
-    img: Img3,
-    text: "Tailandga sayohat",
-    price: "12840000 uzs",
-  },
-  {
-    id: 4,
-    img: Img4,
-    text: "Qatarga sayohat",
-    price: "19700000 uzs",
-  },
-  {
-    id: 5,
-    img: Img5,
-    text: "Antaliyaga sayohat",
-    price: "10200000 uzs",
-  },
-  {
-    id: 6,
-    img: Img6,
-    text: "Ozarbajonga sayohjat",
-    price: "7800000 uzs",
-  },
-  {
-    id: 7,
-    img: Img7,
-    text: "Saudiya Arbistoniga sayohat",
-    price: "17000000 uzs",
-  },
-  {
-    id: 8,
-    img: Img8,
-    text: "Istanbulga sayohat",
-    price: "9660000 uzs",
-  },
-];
-export const Paketlar_data = [
-  {
-    id: 1,
-    img: Paketlar_img1,
-    title: "Dubai/B.A.A",
-    price: "17400000 uzs",
-  },
-  {
-    id: 2,
-    img: Paketlar_img2,
-    title: "Doha/Qatar",
-    price: "19700000 uzs",
-  },
-  {
-    id: 3,
-    img: Paketlar_img3,
-    title: "Antaliya/Turkiya",
-    price: "10200000 uzs",
-  },
-  {
-    id: 4,
-    img: Paketlar_img4,
-    title: "O'zbekiston bo'ylab",
-    price: "48000000 uzs",
-  },
-  {
-    id: 5,
-    img: Paketlar_img5,
-    title: "Phuket/Tayland",
-    price: "12840000 uzs",
-  },
-  {
-    id: 6,
-    img: Paketlar_img6,
-    title: "Istanbul/Turkiya",
-    price: "96600000 uzs",
-  },
-  {
-    id: 7,
-    img: Paketlar_img7,
-    title: "Al-Riyod/Saudiya Arabistoni",
-    price: "17000000 uzs",
-  },
-];
+    {
+        id: 1,
+        img: Img1,
+        text: "O'zbekiston bo'ylab sayohat",
+        price: "4,800,000 uzs",
+        real_price:4800000 ,
+    },
+    {
+        id: 2,
+        img: Img2,
+        text: "Dubaiga sayohat",
+        price: "17,400,000 uzs",
+        real_price:17400000,
+    },
+    {
+        id: 3,
+        img: Img3,
+        text: "Tailandga sayohat",
+        price: "12,840,000 uzs",
+        real_price:12840000,
+    },
+    {
+        id: 4,
+        img: Img4,
+        text: "Qatarga sayohat",
+        price: "19,700,000 uzs",
+        real_price:19700000,
+    },
+    {
+        id: 5,
+        img: Img5,
+        text: "Antaliyaga sayohat",
+        price: "10,200,000 uzs",
+        real_price:10200000,
+    },
+    {
+        id: 6,
+        img: Img6,
+        text: "Ozarbajonga sayohjat",
+        price: "7,800,000 uzs",
+        real_price:7800000,
+    },
+    {
+        id: 7,
+        img: Img7,
+        text: "Saudiya Arbistoniga sayohat",
+        price: "17,000,000 uzs",
+        real_price:17000000,
+    },
+    {
+        id: 8,
+        img: Img8,
+        text: "Istanbulga sayohat",
+        price: "9,660,000 uzs",
+        real_price:9660000,
+    }
+]
